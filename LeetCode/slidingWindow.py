0e4a7ac8998876b724dbb07071c3694f899e41f1
HARSHIT TYAGI <harshittyagi@Hari.local>
2023-03-30 10:55:19 +0530

    3 Longest Substring

--- LeetCode/slidingWindow.py
+++ LeetCode/slidingWindow.py
@@ -1,4 +1,10 @@
-#121. Best Time to Buy and Sell Stock 
+'''
+git add .
+git commit -m "message OR comment"
+git push
+'''
+
+#121. Best Time to Buy and Sell Stock
 #M:O(1) T:O(N)
 def maxProfit(self, prices: List[int]) -> int:
         l,r=0,1 #left=buy, right=sell
@@ -12,3 +18,17 @@ def maxProfit(self, prices: List[int]) -> int:
                 l=r
             r+=1
         return maxP
+
+#3. Longest Substring Without Repeating Characters
+#M:O(N) T:O(N)
+def lengthOfLongestSubstring(self, s: str) -> int:
+    charSet=set()
+    l=0
+    res=0
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l+=1
+        charSet.add(s[r])
+        res=max(res,r-l+1)
+    return res
